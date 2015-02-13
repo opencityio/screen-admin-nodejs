e5dd938f6e38f01ab1c5c56f7a7b3ed7254dbeb2
Josh Brown <joshua.brown@comparethemarket.com>
2015-02-13 10:49:52 +0000

    Added authentication

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,23 @@ var app = global.app,
       res.status(404).send('404');
     };
 
+
 // Heartbeat
 app.get('/heartbeat', heartbeat);
+
+// Authentication
+var auth = require('http-auth');
+var basic = auth.basic({
+  realm: 'Open City'
+}, function (username, password, callback) { // Custom authentication method.
+  callback(username === 'OpenCity' && password === 'test');
+});
+
+app.use(auth.connect(basic));
+
+app.get('/', function(req, res){
+  res.send('Index');
+});
 
 // 404 if no file or route is found
 app.use(fileNotFound)
